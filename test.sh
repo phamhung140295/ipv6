16c6a7d484aede31ee46a941f2e56f1db7034056
phamhung140295 <phamhung140295@gmail.com>
2022-09-14 23:42:18 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ random() {
 }
 IP4=$(curl -4 -s icanhazip.com)
 IP6=2a09:7c41
-IP6_2=1cc4
+LAST_IP6=1cc4
 FIRST_PORT=10000
 LAST_PORT=10100
 
@@ -14,7 +14,7 @@ gen64() {
 	ip64() {
 		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
 	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64):$(ip64):1cc4"
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64):$(ip64):$2"
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
