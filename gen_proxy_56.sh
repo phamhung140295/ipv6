c5f15ba0ead384d6f7216f53b826577436a768c7
phamhung140295 <phamhung140295@gmail.com>
2025-08-04 21:44:44 +0700

    Add files via upload

---
+++ gen_proxy_56.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+random() {
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
+}
+
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+main_interface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
+
+gen56() {
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	# Lấy 3 nhóm đầu, random nhóm thứ 4 trong phạm vi /56, random 4 nhóm cuối
+	prefix=$(echo $1 | cut -d':' -f1-3)
+	segment4=$((RANDOM % 256 + 17920))  # Tạo giá trị từ 4600 đến 46FF
+	hex_segment4=$(printf "%x" $segment4)  # Chuyển số thành dạng hex
+	echo "$prefix:$hex_segment4:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
+
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "$IP4/$port/$(gen56 $IP6)"
+    done
+}
+
+gen_iptables() {
+    cat <<EOF
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $2 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
+}
+
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $3 "/64"}' ${WORKDATA})
+EOF
+}
+
+gen_3proxy() {
+    cat <<EOF
+daemon
+maxconn 2000
+nserver 1.1.1.1
+nserver 8.8.4.4
+nserver 2001:4860:4860::8888
+nserver 2001:4860:4860::8844
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+stacksize 6291456 
+auth strong
+
+users usrproxy:CL:passwdproxy
+
+$(awk -F "/" '{print "auth strong\n" \
+"proxy -6 -n -a -p" $2 " -i" $1 " -e"$3"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
+}
+
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+
+FIRST_PORT=10000
+LAST_PORT=13000
+
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
