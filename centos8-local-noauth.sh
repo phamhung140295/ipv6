c5c5392cdcba05837802dd1a3219e9fa5ec8ba45
phamhung140295 <phamhung140295@gmail.com>
2025-09-02 23:32:37 +0700

    Update centos8-local-noauth.sh

--- centos8-local-noauth.sh
+++ centos8-local-noauth.sh
@@ -86,8 +86,7 @@ $(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $5 "/64"}' ${WORKDA
 EOF
 }
 echo "installing apps"
-yum -y install wget
-yum -y install gcc net-tools bsdtar zip make >/dev/null
+yum -y install wget gcc net-tools bsdtar zip make >/dev/null
 
 install_3proxy
 
